6e11791aa5e5ee46264a432428e92d426ac1a16f
Fellipe <araujo.fellipe@hotmail.com>
2021-07-28 14:21:14 -0300

    Config project to use TypeScript.

---
+++ App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
+import AppLoading from 'expo-app-loading';
+
+import theme from './src/global/styles/theme';
+import { Dashboard } from './src/screens/Dashboard';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      
+      <Dashboard />
+    </ThemeProvider>
+  );
+}
