df0e7f5e46546e8cdeb0c954bc166526306188d0
Fellipe <araujo.fellipe@hotmail.com>
2021-07-29 00:03:29 -0300

    Created and developed Register page.

---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import {
+  Container,
+  Header,
+  Title,
+  Form,
+  Fields,
+  TrasactionTypes,
+} from './styles';
+import { Input } from '../../components/Forms/Input';
+import { Button } from '../../components/Forms/Button';
+import { TransactionTypeButton } from '../../components/Forms/TransactionTypeButton';
+import { CategorySelect } from '../../components/Forms/CategorySelect';
+
+export function Register() {
+  const [transactionType, setTransactionType] = useState('');
+
+  function handleTransactionTypeSelect(type: 'up' | 'down') {
+    setTransactionType(type);
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Title>Cadastro</Title>
+      </Header>
+
+      <Form>
+        <Fields>
+          <Input placeholder="Nome" />
+          <Input placeholder="Preço" />
+
+          <TrasactionTypes>
+            <TransactionTypeButton
+              type="up"
+              title="Income"
+              onPress={() => handleTransactionTypeSelect('up')}
+              isActive={transactionType === 'up'}
+            />
+            <TransactionTypeButton
+              type="down"
+              title="Outcome"
+              onPress={() => handleTransactionTypeSelect('down')}
+              isActive={transactionType === 'down'}
+            />
+          </TrasactionTypes>
+
+          <CategorySelect title="Categoria" />
+        </Fields>
+
+        <Button title="Enviar" />
+      </Form>
+    </Container>
+  );
+}
