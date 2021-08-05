27284eba9fa66cc3b58b2df59515a0e7c8b1418f
Fellipe <araujo.fellipe@hotmail.com>
2021-08-05 00:10:46 -0300

    Renamed component: CategorySelect to CategorySelectButton.

--- src/components/Forms/CategorySelectButton/index.tsx
+++ src/components/Forms/CategorySelectButton/index.tsx
@@ -3,11 +3,12 @@ import { Container, Category, Icon } from './styles';
 
 interface Props {
   title: string;
+  onPress: () => void;
 }
 
-export function CategorySelect({ title }: Props) {
+export function CategorySelectButton({ title, onPress }: Props) {
   return (
-    <Container>
+    <Container onPress={onPress}>
       <Category>{title}</Category>
       <Icon name="chevron-down" />
     </Container>
