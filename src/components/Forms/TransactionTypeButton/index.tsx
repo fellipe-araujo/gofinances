f689d72981832a4c0dbe063f23c417cb16562ee4
Fellipe <araujo.fellipe@hotmail.com>
2021-08-06 01:02:21 -0300

    Changed TouchableOpacity to RectButton on TransactionTypeButton component and refactored this component.

--- src/components/Forms/TransactionTypeButton/index.tsx
+++ src/components/Forms/TransactionTypeButton/index.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import { TouchableOpacityProps } from 'react-native';
-import { Container, Icon, Title } from './styles';
+import { RectButtonProps } from 'react-native-gesture-handler';
+import { Container, Button, Icon, Title } from './styles';
 
 const icons = {
-  up: 'arrow-up-circle',
-  down: 'arrow-down-circle',
-}
+  positive: 'arrow-up-circle',
+  negative: 'arrow-down-circle',
+};
 
-interface Props extends TouchableOpacityProps {
-  type: 'up' | 'down';
+interface Props extends RectButtonProps {
+  type: 'positive' | 'negative';
   title: string;
   isActive: boolean;
 }
 
-export function TransactionTypeButton({ type, title, isActive, ...rest }: Props) {
+export function TransactionTypeButton({
+  type,
+  title,
+  isActive,
+  ...rest
+}: Props) {
   return (
-    <Container {...rest} isActive={isActive} type={type}>
-      <Icon name={icons[type]} type={type} />
-      <Title>{title}</Title>
+    <Container isActive={isActive} type={type}>
+      <Button {...rest}>
+        <Icon name={icons[type]} type={type} />
+        <Title>{title}</Title>
+      </Button>
     </Container>
   );
-};
+}
--- src/components/Forms/TransactionTypeButton/styles.ts
+++ src/components/Forms/TransactionTypeButton/styles.ts
@@ -1,38 +1,46 @@
 import styled, { css } from 'styled-components/native';
-import { TouchableOpacity } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 interface IconsProps {
-  type: 'up' | 'down';
+  type: 'positive' | 'negative';
 }
 
 interface ContainerProps {
   isActive: boolean;
-  type: 'up' | 'down';
+  type: 'positive' | 'negative';
 }
 
-export const Container = styled(TouchableOpacity)<ContainerProps>`
+export const Container = styled.View<ContainerProps>`
   width: 48%;
 
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-
-  border-width: ${({ isActive, type }) => isActive ? 0 : 1.5}px;
+  border-width: ${({ isActive }) => (isActive ? 0 : 1.5)}px;
   border-style: solid;
   border-color: ${({ theme }) => theme.colors.text};
   border-radius: 5px;
 
+  ${({ isActive, type }) =>
+    isActive &&
+    type === 'positive' &&
+    css`
+      background-color: ${({ theme }) => theme.colors.success_light};
+    `}
+
+  ${({ isActive, type }) =>
+    isActive &&
+    type === 'negative' &&
+    css`
+      background-color: ${({ theme }) => theme.colors.attention_light};
+    `}
+`;
+
+export const Button = styled(RectButton)`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
   padding: 16px;
-
-  ${({ isActive, type }) => isActive && type === 'up' && css`
-    background-color: ${({ theme }) => theme.colors.success_light};
-  `}
-
-  ${({ isActive, type }) => isActive && type === 'down' && css`
-    background-color: ${({ theme }) => theme.colors.attention_light};
-  `}
 `;
 
 export const Icon = styled(Feather)<IconsProps>`
@@ -40,7 +48,7 @@ export const Icon = styled(Feather)<IconsProps>`
   margin-right: 12px;
 
   color: ${({ theme, type }) =>
-    type === 'up' ? theme.colors.success : theme.colors.attention};
+    type === 'positive' ? theme.colors.success : theme.colors.attention};
 `;
 
 export const Title = styled.Text`
