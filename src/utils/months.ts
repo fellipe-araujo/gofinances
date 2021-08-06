66b60f56bf9b000851e1063d6fb084f1fb443fd9
Fellipe <araujo.fellipe@hotmail.com>
2021-08-06 00:56:55 -0300

    Added months data.

---
+++ src/utils/months.ts
@@ -0,0 +1,14 @@
+export const monthNames = [
+  'janeiro',
+  'fevereiro',
+  'março',
+  'abril',
+  'maio',
+  'junho',
+  'julho',
+  'agosto',
+  'setembro',
+  'outubro',
+  'novembro',
+  'dezembro',
+];
