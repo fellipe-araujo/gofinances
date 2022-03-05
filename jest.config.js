99a00c5fc4a1cf63d09ae0a65b98760aa2a38ef8
Fellipe Araujo <araujo.fellipe@hotmail.com>
2022-03-05 11:33:31 -0300

    Update jest config

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'jest-expo',
+  testPathIgnorePatterns: ['/node_modules', '/android', '/ios'],
+  setupFilesAfterEnv: [
+    '@testing-library/jest-native/extend-expect',
+    'jest-styled-components',
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.tsx'],
+  coverageReporters: ['lcov'],
+};
